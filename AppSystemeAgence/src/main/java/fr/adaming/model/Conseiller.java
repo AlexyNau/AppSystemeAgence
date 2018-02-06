5f52033b93247a2d90cffa3c632d6ee0745ab9ec
emegu <emegu@192.168.1.173>
2018-02-06 15:22:06 +0100

    emeric commit : client + conseiller /2

---
+++ AppSystemeAgence/src/main/java/fr/adaming/model/Conseiller.java
@@ -0,0 +1,83 @@
+package fr.adaming.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conseiller {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_conseiller")
+	private int id;
+	
+	private String nom;
+	
+	private String mail;
+	
+	private String mdp;
+
+	public Conseiller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Conseiller( String nom, String mail, String mdp) {
+		super();
+		this.nom = nom;
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public Conseiller(int id, String nom, String mail, String mdp) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	@Override
+	public String toString() {
+		return "Conseiller [id=" + id + ", nom=" + nom + ", mail=" + mail + ", mdp=" + mdp + "]";
+	}
+	
+	
+	
+
+}
