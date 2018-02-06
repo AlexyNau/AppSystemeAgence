7b6b27c91d812b3deda6ccc2f6484d9011a6bb00
Adaming44 <Adaming44@192.168.1.145>
2018-02-06 14:43:22 +0100

    commit cammile

--- AppSystemeAgence/src/main/java/fr/adaming/model/Adresse.java
+++ AppSystemeAgence/src/main/java/fr/adaming/model/Adresse.java
@@ -1,5 +1,61 @@
 package fr.adaming.model;
 
-public class Adresse {
+import java.io.Serializable;
+
+@Embeddable
+public class Adresse implements Serializable {
+	
+	private String rue;
+	private int numero;
+	private String ville;
+	private String pays;
+	
+	
+	public Adresse() {
+		super();
+	}
+
+
+	public String getRue() {
+		return rue;
+	}
+
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	public String getPays() {
+		return pays;
+	}
+
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	
+	
+	
 
 }
