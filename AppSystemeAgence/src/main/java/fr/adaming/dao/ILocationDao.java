bb5cc98dcc2882bfa2ada1aa3a4ea64ed228ac73
Adaming44 <Adaming44@192.168.1.145>
2018-02-07 10:16:17 +0100

    Camille : gérer les biens immo CRUD DAO (début)

---
+++ AppSystemeAgence/src/main/java/fr/adaming/dao/ILocationDao.java
@@ -0,0 +1,18 @@
+package fr.adaming.dao;
+
+import java.util.List;
+import fr.adaming.model.Location;
+
+public interface ILocationDao {
+
+	public List<Location> getAllLocation();
+
+	public Location getLocationById(int idLoc);
+	
+	public Location updateLocation(Location loc);
+	
+	public Location addLocation(Location loc);
+	
+	public int deleteLocation(int idLoc);
+
+}
---
+++ AppSystemeAgence/src/main/java/fr/adaming/dao/IVenteDao.java
@@ -0,0 +1,18 @@
+package fr.adaming.dao;
+
+import java.util.List;
+import fr.adaming.model.Vente;
+
+public interface IVenteDao {
+	
+	public List<Vente> getAllVente();
+
+	public Vente getVenteById(int idVente);
+	
+	public Vente updateVente(Vente vente);
+	
+	public Vente addVente(Vente vente);
+	
+	public int deleteVente(int idVente);
+
+}
